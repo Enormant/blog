d72169dd87cc5ec83a56724a2adff588b98a164e
Swayam Siddha <swayamsiddha7@gmail.com>
2021-12-24 20:12:23 +0000

    factories for generating dummy entries or database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,64 +19,25 @@ class DatabaseSeeder extends Seeder
     {
 
 
-        //clean up tables
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
+      
         $user = User::factory() -> create();
 
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        Post::factory(5) -> create([
+            'user_id' => $user->id
         ]);
 
-        $tech = Category::create([
-            'name' => 'Technology',
-            'slug' => 'tech'
+        $user2 = User::factory() -> create();
+
+        Post::factory(5) -> create([
+            'user_id' => $user2->id
         ]);
 
-        $hobby = Category::create([
-            'name' => 'Hobby',
-            'slug' => 'hobby'
-        ]);
+        $user3 = User::factory() -> create();
 
-        //posts
+        Post::factory(5) -> create([
+            'user_id' => $user3->id
+        ]);
         
-        Post::create([
-            'user_id'=> $user -> id,
-            'category_id' => $personal -> id,
-            'title' => 'Post 1',
-            'excerpt' => 'this is the excerpt for post 1',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eu tortor blandit, gravida purus eu, commodo risus. Pellentesque sapien lectus, euismod nec feugiat at, cursus a massa. Integer interdum dui justo, sit amet pretium eros facilisis vitae. Phasellus mollis est nec purus fermentum, nec commodo eros malesuada. Nulla a venenatis nisi. Fusce vestibulum ullamcorper risus. Proin sit amet justo nec turpis ultricies mattis. Vivamus fringilla erat eget orci hendrerit pellentesque. Morbi ipsum mi, tincidunt nec iaculis ut, blandit eget ex. Etiam non felis orci. Pellentesque feugiat velit quis congue mollis. Vivamus vel ullamcorper arcu. Etiam ac quam sit amet diam tristique sollicitudin convallis sed turpis. Suspendisse laoreet sem et nibh tempus, et elementum tortor laoreet. Integer faucibus felis non pretium ultrices. Vestibulum varius at est ut ullamcorper. Donec faucibus volutpat ipsum, at hendrerit dui commodo in. Praesent ut ultrices massa. Cras sed facilisis turpis.',
-            'published_at' => date("Y-m-d H:i:s")
-        ]);
-
-        Post::create([
-            'user_id'=> $user -> id,
-            'category_id' => $hobby -> id,
-            'title' => 'Post 2',
-            'excerpt' => 'this is the excerpt for post 2',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eu tortor blandit, gravida purus eu, commodo risus. Pellentesque sapien lectus, euismod nec feugiat at, cursus a massa. Integer interdum dui justo, sit amet pretium eros facilisis vitae. Phasellus mollis est nec purus fermentum, nec commodo eros malesuada. Nulla a venenatis nisi. Fusce vestibulum ullamcorper risus. Proin sit amet justo nec turpis ultricies mattis. Vivamus fringilla erat eget orci hendrerit pellentesque. Morbi ipsum mi, tincidunt nec iaculis ut, blandit eget ex. Etiam non felis orci. Pellentesque feugiat velit quis congue mollis. Vivamus vel ullamcorper arcu. Etiam ac quam sit amet diam tristique sollicitudin convallis sed turpis. Suspendisse laoreet sem et nibh tempus, et elementum tortor laoreet. Integer faucibus felis non pretium ultrices. Vestibulum varius at est ut ullamcorper. Donec faucibus volutpat ipsum, at hendrerit dui commodo in. Praesent ut ultrices massa. Cras sed facilisis turpis.',
-            'published_at' => date("Y-m-d H:i:s")
-        ]);
-
-        Post::create([
-            'user_id'=> $user -> id,
-            'category_id' => $tech -> id,
-            'title' => 'Post 3',
-            'excerpt' => 'this is the excerpt for post 3',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eu tortor blandit, gravida purus eu, commodo risus. Pellentesque sapien lectus, euismod nec feugiat at, cursus a massa. Integer interdum dui justo, sit amet pretium eros facilisis vitae. Phasellus mollis est nec purus fermentum, nec commodo eros malesuada. Nulla a venenatis nisi. Fusce vestibulum ullamcorper risus. Proin sit amet justo nec turpis ultricies mattis. Vivamus fringilla erat eget orci hendrerit pellentesque. Morbi ipsum mi, tincidunt nec iaculis ut, blandit eget ex. Etiam non felis orci. Pellentesque feugiat velit quis congue mollis. Vivamus vel ullamcorper arcu. Etiam ac quam sit amet diam tristique sollicitudin convallis sed turpis. Suspendisse laoreet sem et nibh tempus, et elementum tortor laoreet. Integer faucibus felis non pretium ultrices. Vestibulum varius at est ut ullamcorper. Donec faucibus volutpat ipsum, at hendrerit dui commodo in. Praesent ut ultrices massa. Cras sed facilisis turpis.',
-            'published_at' => date("Y-m-d H:i:s")
-        ]);
-
-        Post::create([
-            'user_id'=> $user -> id,
-            'category_id' => $personal -> id,
-            'title' => 'Post 4',
-            'excerpt' => 'this is the excerpt for post 4',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eu tortor blandit, gravida purus eu, commodo risus. Pellentesque sapien lectus, euismod nec feugiat at, cursus a massa. Integer interdum dui justo, sit amet pretium eros facilisis vitae. Phasellus mollis est nec purus fermentum, nec commodo eros malesuada. Nulla a venenatis nisi. Fusce vestibulum ullamcorper risus. Proin sit amet justo nec turpis ultricies mattis. Vivamus fringilla erat eget orci hendrerit pellentesque. Morbi ipsum mi, tincidunt nec iaculis ut, blandit eget ex. Etiam non felis orci. Pellentesque feugiat velit quis congue mollis. Vivamus vel ullamcorper arcu. Etiam ac quam sit amet diam tristique sollicitudin convallis sed turpis. Suspendisse laoreet sem et nibh tempus, et elementum tortor laoreet. Integer faucibus felis non pretium ultrices. Vestibulum varius at est ut ullamcorper. Donec faucibus volutpat ipsum, at hendrerit dui commodo in. Praesent ut ultrices massa. Cras sed facilisis turpis.',
-            'published_at' => date("Y-m-d H:i:s")
-        ]);
+     
     }
 }
