319b8b8462595c6a845065372180258810369bee
Swayam Siddha <swayamsiddha7@gmail.com>
2021-12-26 18:41:49 +0000

    dummy post comment component

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,16 @@
+<div class='flex bg-gray-100 border border-gray-200 rounded-xl p-6 my-6'>
+    <div class='mr-2'>
+        <img src="https://i.pravatar.cc/100" alt= "" class='rounded-xl'/>
+    </div>
+    <div>
+        <header>
+            <h3 class='font-bold'>John Doe </h3>
+            <p class='text-xs'> posted 8 months ago </p> 
+        </header>
+        <p>
+            Optio temporibus aut est molestiae qui qui. Ipsum tempore quae vero numquam cumque numquam dolorum. Officiis rerum non quo qui. Optio temporibus aut est molestiae qui qui. Ipsum tempore quae vero numquam cumque numquam dolorum. Officiis rerum non quo qui.
+            Optio temporibus aut est molestiae qui qui. Ipsum tempore quae vero numquam cumque numquam dolorum. Officiis rerum non quo qui.
+
+        </p>
+    </div>
+</div>
